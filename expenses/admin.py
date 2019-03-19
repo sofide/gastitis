62569c73ee168a57b1040a8ec92abcaa53124f19
Sofia Denner <sofi.denner@gmail.com>
2019-03-19 00:30:58 -0300

    add expenses models

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from expenses.models import Expense, Category, Division
+
+
+admin.site.register(Expense)
+admin.site.register(Category)
+admin.site.register(Division)
--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Expenses categories, to keep track grouped expensives, and compare them in different periods.
+    """
+    name = models.CharField(max_length=256)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Expense(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    description = models.TextField()
+    amount = models.DecimalField(decimal_places=2, max_digits=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    date = models.DateField()
+    created_date = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date', 'amount']
+
+    def __str__(self):
+        return '{} - ${} - {}'.format(self.date, self.amount, self.description)
+
+
+class Division(models.Model):
+    """
+    How much should everyone pay to afford the total expenses.
+    """
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    portion = models.FloatField()
+
+    def __str__(self):
+        return '{} - %{}'.format(self.user, self.portion)
