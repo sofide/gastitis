afdbac6605f2bf59ac57c81c96aff2b711f9d8cb
Sofia Denner <sofi.denner@gmail.com>
2019-03-30 22:03:44 -0300

    disable csrf for webhook view

--- bot/views.py
+++ bot/views.py
@@ -3,10 +3,13 @@ import json
 from django.conf import settings
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 from bot.bot import Bot
 
 
+@csrf_exempt
 def webhook(request, token):
     if not token == settings.BOT_TOKEN:
         raise Http404()
