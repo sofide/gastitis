51880838a6a804cdabcc98746c2b201ecc83ce40
Sofia Denner <sofi.denner@gmail.com>
2019-04-02 18:48:14 -0300

    add telegram group model

--- bot/models.py
+++ bot/models.py
@@ -8,3 +8,12 @@ class TelegramUser(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='telegram')
     chat_id = models.IntegerField()
     username = models.CharField(max_length=256, blank=True)
+
+
+class TelegramGroup(models.Model):
+    users = models.ManyToManyField('auth.User', related_name='telegram_groups')
+    chat_id = models.IntegerField()
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
