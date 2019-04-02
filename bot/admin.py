9ecc3074b772126c442c683f5297669e5a8b18ec
Sofia Denner <sofi.denner@gmail.com>
2019-04-02 18:50:24 -0300

    register bot models in admin

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from bot.models import TelegramGroup, TelegramUser
+
 # Register your models here.
+admin.site.register(TelegramGroup)
+admin.site.register(TelegramUser)
