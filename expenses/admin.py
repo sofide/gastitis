f1a917ec72ac08da189cf710e8aba089e2a6047b
Sofia Denner <sofi.denner@gmail.com>
2019-04-21 11:41:21 -0300

    improve expenses admin site

--- expenses/admin.py
+++ expenses/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from expenses.models import Expense, Tag, Division
 
 
-admin.site.register(Expense)
 admin.site.register(Tag)
 admin.site.register(Division)
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'amount', 'group', 'description')
