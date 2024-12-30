9f2ea1bb402241f5476ae087d38175bb6ed2f380
Sofía Denner <sofi.denner@gmail.com>
2024-12-30 11:47:13 -0300

    remove deprecated code

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,8 @@
+"""
+Initialize telegram bot and start polling messages.
+"""
+
 from django.conf import settings
-from telegram import Bot as TelegramBot, Update
 from telegram.ext import Application
 
 from bot.handlers import HANDLERS
@@ -8,9 +11,6 @@ from bot.handlers import HANDLERS
 class Bot:
     def __init__(self, token=settings.BOT_TOKEN):
         self.application = Application.builder().token(token).build()
-        self.bot = TelegramBot(token)
-        self.dispatcher = None
-
 
         for handler in HANDLERS:
             self.application.add_handler(handler)
